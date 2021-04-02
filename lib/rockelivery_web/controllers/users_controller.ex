b222c09c216b63092287ee7420d2e453a0caabd5
Alesandro Silva <silva_alesandro@outlook.com>
2021-04-02 16:08:28 -0400

    feat: adapt fallback_controler to receive status and result from actions

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -1,11 +1,13 @@
 defmodule RockeliveryWeb.UsersController do
   use RockeliveryWeb, :controller
+  alias RockeliveryWeb.FallbackController
   alias Rockelivery.Users.User
-
+  
+  action_fallback FallbackController
   def create(conn, params) do
     with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
       conn
-      |> put_status(:application)
+      |> put_status(:created)
       |> render("create.json", user: user)
     end
   end
