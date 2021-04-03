9341b5e333b514a04d84fc7d1d5a2e79ac5bacf2
Alesandro Silva <silva_alesandro@outlook.com>
2021-04-02 20:19:08 -0400

    feat: add get_user_by_id in main facade

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,5 +1,8 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UserCreate
+  alias Rockelivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate get_user(id), to: UserGet, as: :by_id
 end
