10b595ae7ea5dc664dcbd95e261637b7dc30a634
Alesandro Silva <silva_alesandro@outlook.com>
2021-05-03 08:40:18 -0400

    feat: add create order module

--- lib/rockelivery/orders/create.ex
+++ lib/rockelivery/orders/create.ex
@@ -1,8 +1,18 @@
 defmodule Rockelivery.Orders.Create do
+  import Ecto.Query
   alias Rockelivery.{Error, Repo}
   alias Rockelivery.Items.Item
 
   def call(params) do
     params
+    |> fetch_items()
+  end
+
+  defp fetch_items(%{"items" => items_params}) do
+    items_ids = Enum.map(items_params, fn item -> item["id"] end)
+
+    query = from item in Item, where: item.id in ^items_ids
+
+    Repo.all(query)
   end
 end
